b2f8e0b5f98c9f60de04aa4a9ca740347e042092
王家强 <wangjiaqiang@hive-intel.com>
2021-09-13 15:55:21 +0800

    :label: 新增关于页面

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// 一个标准的可执行的 Go 程序必须有 package main 的声明。
 package main
 
 import (
@@ -6,7 +7,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
 
 func main() {
